2da900683a3fb7f1c20e0bc1afbd2f38a2cd16fa
leo <laahiri1684@gmail.com>
2024-07-17 10:29:55 +0530

    first

---
+++ app/src/main/java/com/example/unitconverter/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.unitconverter;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+public class MainActivity extends AppCompatActivity {
+
+    CardView cv_tmp;
+    CardView cv_weight;
+    CardView cv_length;
+    CardView cv_speed;
+    CardView cv_volume;
+    CardView cv_time;
+    CardView cv_area;
+    CardView cv_fuel;
+    CardView cv_pressure;
+    CardView cv_energy;
+    CardView cv_storage;
+    CardView cv_current;
+    CardView cv_force;
+    CardView cv_freq;
+    CardView cv_resistance;
+    CardView cv_power;
+    CardView cv_torque;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cv_tmp = findViewById(R.id.cv_tmp);
+        cv_weight = findViewById(R.id.cv_weight);
+        cv_length = findViewById(R.id.cv_length);
+        cv_speed = findViewById(R.id.cv_speed);
+        cv_volume = findViewById(R.id.cv_Volume);
+
+        cv_freq = findViewById(R.id.cv_frequency);
+
+
+        cv_tmp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, temp_cal.class));
+            }
+        });
+        cv_weight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, weight_cal.class));
+            }
+        });
+        cv_length.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, length_cal.class));
+            }
+        });
+        cv_speed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, speed_cal.class));
+            }
+        });
+        cv_volume.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, volume_cal.class));
+            }
+        });
+
+        cv_freq.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, freq_cal.class));
+            }
+        });
+
+    }
+}
